c562c0f339dba13fae19fda8ebe4b900c2484dfa
Mister-Corn <code@ronaldlibago.com>
2018-12-25 12:04:56 -0800

    Complete unit test exercise

--- server/src/utils/__tests__/auth.todo.js
+++ server/src/utils/__tests__/auth.todo.js
@@ -1,6 +1,29 @@
-test('isPasswordAllowed only allows some passwords', () => {
-  // here's where I'll demo things for you :)
+import {isPasswordAllowed, userToJSON} from '../auth'
+
+// TODO: refactor
+describe('isPasswordAllowed', () => {
+  const allowedPasswords = ['sfkl.e903f.s']
+  const disallowedPasswords = ['', 'sfk.9', 'ffffffff', '88888888']
+
+  allowedPasswords.forEach(pwd => {
+    it(`"${pwd}" should be allowed`, () => {
+      expect(isPasswordAllowed(pwd)).toBe(true)
+    })
+  })
+  disallowedPasswords.forEach(pwd => {
+    it(`"${pwd}" should be disallowed`, () => {
+      expect(isPasswordAllowed(pwd)).toBe(false)
+    })
+  })
 })
+
+// test('isPasswordAllowed only allows some passwords', () => {
+//   // here's where I'll demo things for you :)]
+//   expect(isPasswordAllowed('')).toBe(false)
+//   expect(isPasswordAllowed('fffffffff')).toBe(false)
+//   expect(isPasswordAllowed('888888888')).toBe(false)
+//   expect(isPasswordAllowed('sk3.eq91.y4s')).toBe(true)
+// })
 
 test('userToJSON excludes secure properties', () => {
   // Here you'll need to create a test user object
@@ -9,19 +32,34 @@ test('userToJSON excludes secure properties', () => {
   // doesn't have any of the properties it's not
   // supposed to.
   // Here's an example of a user object:
-  // const user = {
-  //   id: 'some-id',
-  //   username: 'sarah',
-  //   // ↑ above are properties which should
-  //   // be present in the returned object
-  //
-  //   // ↓ below are properties which shouldn't
-  //   // be present in the returned object
-  //   exp: new Date(),
-  //   iat: new Date(),
-  //   hash: 'some really long string',
-  //   salt: 'some shorter string',
-  // }
+  // Arrange
+  const expected = {
+    id: 'some-id',
+    username: 'sarah',
+  }
+  const user = {
+    ...expected,
+    // ↑ above are properties which should
+    // be present in the returned object
+
+    // ↓ below are properties which shouldn't
+    // be present in the returned object
+    exp: new Date(),
+    iat: new Date(),
+    hash: 'some really long string',
+    salt: 'some shorter string',
+  }
+  const rejected = {
+    exp: expect.anything(),
+    iat: expect.anything(),
+    hash: expect.anything(),
+    salt: expect.anything(),
+  }
+  // Act
+  const result = userToJSON(user)
+  // Assert
+  expect(result).toEqual(expected)
+  expect(result).not.toEqual(expect.objectContaining(rejected))
 })
 
 //////// Elaboration & Feedback /////////
